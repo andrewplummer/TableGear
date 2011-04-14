71ec42c044075333e277124e5ae69982e8041d4e
Andrew Plummer <aplummer@cerego.co.jp>
2011-04-14 16:36:08 +0900

    changed release script

--- scripts/create_release.sh
+++ scripts/create_release.sh
@@ -7,9 +7,9 @@ rm releases/TableGear$1-jQuery.zip
 cd lib/
 
 cat example.php jquery.php > index.php
-zip ../releases/TableGear$1-jQuery.zip index.php javascripts/TableGear$1-jQuery.js include/* stylesheets/* images/*
+zip ../releases/TableGear-jQuery-$1.zip index.php javascripts/tablegear-jquery.js include/* stylesheets/* images/*
 
 cat example.php mootools.php > index.php
-zip ../releases/TableGear$1-MooTools.zip index.php javascripts/TableGear$1-MooTools.js include/* stylesheets/* images/*
+zip ../releases/TableGear-MooTools-$1.zip index.php javascripts/tablegear-mootools.js include/* stylesheets/* images/*
 
 rm index.php
